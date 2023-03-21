564d015696065ce357ddab15fabdde0e3f5a0e84
AhnYeonghoo <dksdudgn00@naver.com>
2023-03-21 09:21:45 +0900

    Singleton Pattern

---
+++ src/Chapter6/Singleton.java
@@ -0,0 +1,26 @@
+package Chapter6;
+
+public class Singleton {
+
+    private static Singleton singleton = new Singleton();
+
+    private Singleton() {
+
+    }
+
+    static Singleton getInstance() {
+        return singleton;
+    }
+
+    public static void main(String[] args) {
+
+        Singleton obj1 = Singleton.getInstance();
+        Singleton obj2 = Singleton.getInstance();
+
+        if (obj1 == obj2) {
+            System.out.println("singleton");
+        } else {
+            System.out.println("no singleton");
+        }
+    }
+}
